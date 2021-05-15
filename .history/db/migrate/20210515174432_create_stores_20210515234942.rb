abc10e2297ed0fe83e495b85fab8065f2dd71bb4
Marwa Medhat <eng.marwamedhat2020@gmail.com>
2021-05-16 01:40:17 +0200

    logout

--- .history/db/migrate/20210515174432_create_stores_20210515234942.rb
+++ .history/db/migrate/20210515174432_create_stores_20210515234942.rb
@@ -5,6 +5,7 @@ class CreateStores < ActiveRecord::Migration[5.2]
       t.text :summary
       t.datetime :published_at
       t.references :user, foreign_key: true
+
       t.timestamps
     end
   end
