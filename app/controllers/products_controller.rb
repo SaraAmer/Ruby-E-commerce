a1311c21ac28c3eefe5958b8c3c4f40a417b6ddf
walaaElbasha <walaaelbasha40@yahoo.com>
2021-05-16 22:53:12 +0200

    avatar photo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,20 @@
 class ProductsController < InheritedResources::Base
-
+ 
   def index
-    
+  @products = Product.all
+ 
+
   end
-
+  
+  
+  def show
+  @products = Product.find(params[:id])
+  end
+  
+  
   private
-
-    def product_params
-      params.require(:product).permit(:name, :store_id, :category_id, :price, :rate, :quantity)
-    end
-
-end
+  def product_params
+  params.require(:product).permit(:name, :store_id, :category_id, :price, :rate, :quantity)
+  end
+  
+ end
