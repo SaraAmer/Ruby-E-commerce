58d89c7736e5abee523669786a44dc3bd850fca0
Sara Amer <saraamersaad8@gmail.com>
2021-05-21 22:55:51 +0200

    add description coloum

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,7 +73,7 @@ class ProductsController < InheritedResources::Base
 
     private
     def product_params
-      params.require(:product).permit(:name, :category_id, :price, :rate, :quantity ,:brand_id, images: [])
+      params.require(:product).permit(:name, :category_id, :price, :rate, :quantity ,:brand_id, :description,images: [] )
     end
 
 end
