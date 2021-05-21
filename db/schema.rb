adc4c262decfe7775ad0257ff8aa1b918f88ce97
walaaElbasha <walaaelbasha40@yahoo.com>
2021-05-21 23:11:34 +0200

    Add to cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_223027) do
+ActiveRecord::Schema.define(version: 2021_05_21_004329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_223027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "subtotal"
+    t.integer "unit_price"
     t.index ["order_id"], name: "index_order_products_on_order_id"
     t.index ["product_id"], name: "index_order_products_on_product_id"
   end
