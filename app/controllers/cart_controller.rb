382db13a045021005a0942dd367744ed469f330b
Sara Amer <saraamersaad8@gmail.com>
2021-05-23 17:24:42 +0200

    total

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,6 +2,10 @@ class CartController < ApplicationController
 def index
     @cart = Cart.find_or_create_by(user: current_user)
     @products = @cart.products
+    @total = 0 ; 
+    @products.each do |product|
+        @total+= product.carts_products[0].quantity*product.price
+    end
 end
 def create
     @product = Product.find(params[:product])
