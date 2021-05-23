6a897cac586fbcdfd90a79358d2bf1b2112bab30
RaghdaMadiane <raghdamadiane@gmail.com>
2021-05-23 23:52:16 +0200

    home

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,9 +71,9 @@ end
  
   if params[:category] != "All" 
     @productCategories=Product.where(category_id: params[:category])
-    @productCategory= @productCategories.all.order("created_at ASC").where("created_at >= ?", Time.now-1.days)
+    @productCategory= @productCategories.all.order("created_at ASC").where("created_at >= ?", Time.now-2.days)
   else
-    @productCategory = Product.all.order("created_at ASC").where("created_at >= ?", Time.now-1.days)
+    @productCategory = Product.all.order("created_at ASC").where("created_at >= ?", Time.now-2.days)
 
     end
  
