d35a6841db1a017320e0b82a4c1ef44918c64050
Eithar-Yahia <eitharyahia444@gmail.com>
2021-05-25 21:45:31 +0200

    finish

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,9 @@ class UserMailer < ApplicationMailer
     ##n7ot al mail bt3na
     default from: 'lifehotel881@gmail.com'
 
-    def welcome_email
-      @user = params[:user]
-      @url  = 'http://example.com/login'
-      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-    end
+    # def welcome_email
+    #   @user = params[:user]
+    #   @url  = 'http://example.com/login'
+    #   mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  after_create :send_email
+  #after_create :send_email
   has_one :cart
   has_one_attached :avatar
   has_one :payment
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-private
-def send_email
-UserMailer.with(user: @user).welcome_email.deliver_now
-end
+# private
+# def send_email
+# UserMailer.with(user: @user).welcome_email.deliver_now
+# end
 
 end
       
